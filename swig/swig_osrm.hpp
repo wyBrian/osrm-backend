9fe22839054f8d468c00c30e61185ce84a031f91
Wang Xian <wangxhere@gmail.com>
2020-04-19 12:19:08 +0800

    swig for JNI

---
+++ swig/swig_osrm.hpp
@@ -0,0 +1,44 @@
+//
+// Created by Wang Xian on 2020/4/19.
+//
+
+#ifndef OSRM_SWIG_OSRM_HPP
+#define OSRM_SWIG_OSRM_HPP
+
+#include "util/exception_utils.hpp"
+#include "util/log.hpp"
+#include "util/meminfo.hpp"
+#include "util/typedefs.hpp"
+#include "util/version.hpp"
+
+#include "osrm/engine_config.hpp"
+#include "osrm/exception.hpp"
+#include "osrm/osrm.hpp"
+#include "osrm/storage_config.hpp"
+
+#include "storage/serialization.hpp"
+#include "storage/shared_memory.hpp"
+#include "storage/shared_monitor.hpp"
+#include "storage/storage.hpp"
+
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+
+#include <boost/algorithm/string/case_conv.hpp>
+#include <boost/any.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+
+#include <csignal>
+#include <cstdlib>
+
+#include <chrono>
+#include <exception>
+#include <future>
+#include <iostream>
+#include <memory>
+#include <new>
+#include <string>
+
+
+#endif //OSRM_SWIG_OSRM_HPP
